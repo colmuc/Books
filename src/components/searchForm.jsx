fb2f01c1cc7a0831b1bf3d356464677ca8419763
Collins Mucyo <collinsmucyo@gmail.com>
2019-05-01 04:06:35 -0500

    Added error handling in case of bad input

--- src/components/searchForm.jsx
+++ src/components/searchForm.jsx
@@ -3,6 +3,8 @@ import Joi from "joi-browser";
 import Form from "../common/form";
 import httpService from "../services/httpService";
 import config from "../config.json";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 class SearchForm extends Form {
   state = {
@@ -38,45 +40,55 @@ class SearchForm extends Form {
     if (this.state.books.length === 0) {
       return <p className="container">Results will appear here!</p>;
     } else {
-      return (
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>Book Image</th>
-              <th>Book Author</th>
-              <th>Book Title</th>
-              <th>Book Publisher</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.books.items.map(book => (
-              <tr key={book.id}>
-                <td>
-                  <img alt="book" src={book.volumeInfo.imageLinks.thumbnail} />
-                </td>
-                <td>{book.volumeInfo.authors}</td>
-                <td>{book.volumeInfo.title}</td>
-                <td>{book.volumeInfo.publisher}</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-info"
-                    onClick={this.onSeeMoreClick.bind(book, book)}
-                  >
-                    See More
-                  </button>
-                </td>
+      try {
+        return (
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>Book Image</th>
+                <th>Book Author</th>
+                <th>Book Title</th>
+                <th>Book Publisher</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      );
+            </thead>
+            <tbody>
+              {this.state.books.items.map(book => (
+                <tr key={book.id}>
+                  <td>
+                    <img
+                      alt="book"
+                      src={book.volumeInfo.imageLinks.thumbnail}
+                    />
+                  </td>
+                  <td>{book.volumeInfo.authors}</td>
+                  <td>{book.volumeInfo.title}</td>
+                  <td>{book.volumeInfo.publisher}</td>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn btn-info"
+                      onClick={this.onSeeMoreClick.bind(book, book)}
+                    >
+                      See More
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        );
+      } catch (ex) {
+        toast.error("Unable to find books");
+        const books = [];
+        this.setState({ books });
+      }
     }
   }
 
   render() {
     return (
       <div className="container">
+        <ToastContainer />
         <form>
           {this.renderInput("query", "Query")}
           {this.renderButton("Search")}
